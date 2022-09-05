67f22f537cf41c37b23b09efd342ef24d0da0418
oguzhanbektas <ozibektas@gmail.com>
2022-09-05 15:31:32 +0300

    .

--- shopping-app/web/src/App.vue
+++ shopping-app/web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button class="btn btn-danger"> Deneme </button>
 </template>
 
 <script>
